8df5813fec23081d241e11c27ff6c87887969a38
Liliane Castro <lcs2@icomp.ufam.edu.br>
2020-08-11 22:39:47 -0400

    feat(Scrolling): Material Scrolling

---
+++ Assets/Scripts/Material/ScrollingBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    private Renderer meshRenderer;
+    private Material currentMaterial;
+    private float offset;
+    public float increaseOffset;
+    public float speed;
+    public int orderLayer;
+    public string sortingLayer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.sortingLayerName = sortingLayer;
+        meshRenderer.sortingOrder = orderLayer;
+        currentMaterial = meshRenderer.material;
+    }
+
+    void FixedUpdate()
+    {
+        offset += increaseOffset;
+        currentMaterial.SetTextureOffset("_MainTex", new Vector2(offset * speed, 0));
+    }
+}
