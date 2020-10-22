d12363381ef30898f5d7177dfdcc68b5ca1a354d
Liliane Castro <lcs2@icomp.ufam.edu.br>
2020-10-21 20:42:46 -0400

    feat(GroundMove): update ground movement

---
+++ Assets/Scripts/Controller/GroundMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMove : MonoBehaviour
+{
+    private Rigidbody2D             groundRb;
+    public float                    difX;
+    public float                    minX;
+    
+    void Start()
+    {
+        groundRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        groundRb.velocity = new Vector2(GameController.Instance.getSpeed(), 0);
+
+        if(transform.position.x <= minX)
+        {
+            transform.position = new Vector3(transform.position.x + difX *2,
+            transform.position.y,
+            transform.position.z);
+        }
+    }
+}
